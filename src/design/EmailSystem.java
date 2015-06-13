5bd461682471b758112c888cac870b47f683f262
farazw808 <Faraz_Waseem@intuit.com>
2015-06-13 16:52:01 -0700

    design questions.

---
+++ src/design/EmailSystem.java
@@ -0,0 +1,9 @@
+package design;
+
+/**
+ * Created by farazw808 on 6/12/15.
+ * Design highly scalable e-mail system for web clients.
+ * data structures, key classes database etc.
+ */
+public class EmailSystem {
+}
---
+++ src/design/PrakingGarage.java
@@ -0,0 +1,11 @@
+package design;
+
+/**
+ * Created by farazw808 on 6/12/15.
+ * Design parking garage.
+ * It should give us closest parking space available.
+ * What data-structure will we use
+ * what are the key classes, interfaces we need.
+ */
+public class PrakingGarage {
+}
